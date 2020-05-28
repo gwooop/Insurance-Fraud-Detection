f993ba78e3fdbaad03c949f1fb0640ac56eb3146
이득규 <dkgulee@github.com>
2020-05-28 09:16:15 +0900

    semi_p

---
+++ InsuranceFraudDetection/src/main/webapp/WEB-INF/resources/assets/js/product/product-json.js
@@ -0,0 +1,259 @@
+    $(".searchBtn").on("click", function(){
+      var checkBtn = $(this);
+      var tr = checkBtn.parent().parent();
+      var td = tr.children();
+      var custId = td.eq(1).text();
+      console.log(custId)
+      $.ajax({
+         url:"<c:url value='/restTest/'/>"+custId,
+         type:"GET",
+         dataType:'json',
+         data:{},
+         timeout:100000,
+         beforeSend: function() {
+            $('.wrap-loading').removeClass('display-none');
+         },complete:function(){
+            $('.wrap-loading').addClass('display-none');  
+         },
+         success:function(result){
+            console.log(result);
+            
+            data = JSON.parse(result[0])
+            data2 = JSON.parse(result[1])
+            data3 = JSON.parse(result[2])
+            data4 = JSON.parse(result[3])
+        
+            Highcharts.chart('container', {
+              chart: {
+                  plotBackgroundColor: null,
+                  plotBorderWidth: null,
+                  plotShadow: false,
+                  type: 'pie'
+              },
+              title: {
+                  text: data[0]["CUST_ID"] + '의 보험 사기자일 확률',
+                  style: {
+                      color: '#007bff',
+                      fontWeight: 900
+                  }
+              },
+              tooltip: {
+                  pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+              },
+              accessibility: {
+                  point: {
+                      valueSuffix: '%'
+                  }
+              },
+              plotOptions: {
+                  pie: {
+                      allowPointSelect: true,
+                      cursor: 'pointer',
+                      colors: pieColors,
+                      dataLabels: {
+                          enabled: true,
+                          format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
+                          distance: -50,
+                          filter: {
+                              property: 'percentage',
+                              operator: '>',
+                              value: 4
+                          },
+                          style:{
+                              fontFamily: 'monospace',
+                            fontSize: '1.2em',
+                            fontWeight: 0
+                          }
+                          
+                      }
+                  }
+              },
+              series: [{
+                  name: '보험사기자',
+                  data: [
+                      { name: '일반인', y: data[0]["NO"] },
+                      { name: '사기꾼', y: data[0]["YES"] }
+                   
+                  ],
+                  animation: {
+                      duration: 3000
+                  }
+                  
+              
+              }]
+          });
+            Highcharts.chart('container2', {
+              chart: {
+                  plotBackgroundColor: null,
+                  plotBorderWidth: null,
+                  plotShadow: false,
+                  type: 'pie'
+              },
+              title: {
+                  text: "SVM",
+                  style: {
+                      color: '#007bff',
+                      fontWeight: 100,
+                      fontSize: '1.2em'
+                  }
+              },
+              tooltip: {
+                  pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+              },
+              accessibility: {
+                  point: {
+                      valueSuffix: '%'
+                  }
+              },
+              plotOptions: {
+                  pie: {
+                      allowPointSelect: true,
+                      cursor: 'pointer',
+                      colors: pieColors,
+                      dataLabels: {
+                          enabled: false,
+                          format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
+                          distance: -50,
+                          filter: {
+                              property: 'percentage',
+                              operator: '>',
+                              value: 4
+                          }
+                      }
+                  }
+              },
+              series: [{
+                  name: '보험사기자',
+                  data: [
+                      { name: '일반인', y: data2[0]["NO"] },
+                      { name: '사기꾼', y: data2[0]["YES"] },
+                  ]
+              }]
+          });
+            Highcharts.chart('container3', {
+              chart: {
+                  plotBackgroundColor: null,
+                  plotBorderWidth: null,
+                  plotShadow: false,
+                  type: 'pie'
+              },
+              title: {
+                  text: "XGBOOST",
+                  style: {
+                      color: '#007bff',
+                      fontWeight: 100,
+                      fontSize: '1.2em'
+                  }
+              },
+              tooltip: {
+                  pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+              },
+              accessibility: {
+                  point: {
+                      valueSuffix: '%'
+                  }
+              },
+              plotOptions: {
+                  pie: {
+                      allowPointSelect: true,
+                      cursor: 'pointer',
+                      colors: pieColors,
+                      dataLabels: {
+                          enabled: false,
+                          format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
+                          distance: -50,
+                          filter: {
+                              property: 'percentage',
+                              operator: '>',
+                              value: 4
+                          }
+                      }
+                  }
+              },
+              series: [{
+                  name: '보험사기자',
+                  data: [
+                      { name: '일반인', y: data3[0]["NO"] },
+                      { name: '사기꾼', y: data3[0]["YES"] },
+                  ]
+              }]
+          });
+           // -----
+            Highcharts.chart('container4', {
+              chart: {
+                  plotBackgroundColor: null,
+                  plotBorderWidth: null,
+                  plotShadow: false,
+                  type: 'pie'
+              },
+              title: {
+                  text: "Random Forest",
+                  style: {
+                      color: '#007bff',
+                      fontWeight: 100,
+                      fontSize: '1.2em'
+                  }
+              },
+              tooltip: {
+                  pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+              },
+              accessibility: {
+                  point: {
+                      valueSuffix: '%'
+                  }
+              },
+              plotOptions: {
+                  pie: {
+                      allowPointSelect: true,
+                      cursor: 'pointer',
+                      colors: pieColors,
+                      dataLabels: {
+                          enabled: false,
+                          format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
+                          distance: -50,
+                          filter: {
+                              property: 'percentage',
+                              operator: '>',
+                              value: 4
+                          }
+                      }
+                  }
+              },
+              series: [{
+                  name: '보험사기자',
+                  data: [
+                      { name: '일반인', y: data4[0]["NO"] },
+                      { name: '사기꾼', y: data4[0]["YES"] },
+                  ]
+              }]
+          });
+         $("#textArea").empty() 
+      if(data[0]["SIU_CUST_YN"] ==0){
+         $("#textArea").append("<h2> 일반인일 확률이 더 높습니다. </h2>")
+      
+         $("#textArea").focus();
+         }else{
+            $("#textArea").append("<h2> 사기꾼일 확률이 더 높습니다. </h2>")
+         } 
+             
+            
+      },
+      error:function(request,status,error){
+           alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+        }
+      })
+     })
+     
+     // Make monochrome colors
+      var pieColors = (function () {
+          var colors = [],
+              base = Highcharts.getOptions().colors[0],
+              i;
+      
+          for (i = 0; i < 10; i += 1) {
+              // Start out with a darkened base color (negative brighten), and end
+              // up with a much brighter color
+              colors.push(Highcharts.color(base).brighten((i) / 5).get());
+          }
+          return colors;
+      }());
---
+++ InsuranceFraudDetection/src/main/webapp/WEB-INF/views/statistic.jsp
@@ -0,0 +1,113 @@
+<%@ page contentType="text/html; charset=utf-8" trimDirectiveWhitespaces="true" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<html lang="en">
+
+<jsp:include page="/WEB-INF/views/include/staticFiles.jsp"/>
+<jsp:include page="/WEB-INF/views/include/header.jsp"/>
+
+<script src="https://www.amcharts.com/lib/4/core.js"></script>
+<script src="https://www.amcharts.com/lib/4/charts.js"></script>
+<script src="https://www.amcharts.com/lib/4/plugins/wordCloud.js"></script>
+<script src="https://www.amcharts.com/lib/4/themes/animated.js"></script>
+<link href="<c:url value='/assets/css/statistic/style.css'/>" rel="stylesheet"/>
+<body>
+   <main id="main">
+
+    <!-- ======= Blog Section ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>statistic</h2>
+
+          <ol>
+            <li><a href="./">Home</a></li>
+            <li>statistic</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Blog Section -->
+
+    <!-- ======= Blog Section ======= -->
+    <section class="blog" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-9 entries">
+			<!-- 첫번쨰 차트 -->
+            <article class="entry" id="chart1">
+				<h2 id="chartText" ></h2>
+				<div id="chartdiv"></div>
+				<h4 id="chartContent"></h4>
+            </article><!-- End blog entry -->
+			<!-- 두번쨰 차트 -->
+            <article class="entry" id="chart2">
+				<h2 id="chartText_sub"></h2>
+				<div id="chartdiv_sub"></div>
+				<h4 id="chartContent_sub"></h4>
+			</article><!-- End blog entry -->
+			<!-- 세번쨰 차트  -->
+			<article class="entry" id="chart3" style="display:none">
+				<div id="selectArea" style="display:none">
+					<select id="test" class="custom-select" style="width:15%">
+  						<option value="1">1</option>
+  						<option value="2">2</option>
+  					</select>
+				</div>
+				<h2 id="chartText_sub2"></h2>
+				<div id="chartdiv_sub2"></div>
+				<h4 id="chartContent_sub2"></h4>
+			</article><!-- End blog entry -->
+			<!-- 네번쨰 차트  -->
+			<article class="entry" id="chart4" style="display:none">
+				<div id="selectArea2" style="display:none">
+					<select id="test2" class="custom-select" style="width:15%">
+  						<option value="1">1</option>
+  						<option value="2">2</option>
+  					</select>
+  				</div>
+				<h2 id="chartText_sub3"></h2>
+				<div id="chartdiv_sub3">
+				</div>
+			<h4 id="chartContent_sub3"></h4>
+			</article><!-- End blog entry -->
+          </div><!-- End blog entries list -->
+		<!-- Start Category -->
+          <div class="col-lg-3">
+          	<div class="sidebar">
+            	<h4 class="sidebar-title">Categories</h4>	
+            	<div class="sidebar-item categories">
+		            <ul>
+		              <li><a href="<c:url value='/statistic'/>">실제 현황</a></li>
+		              <li><a id="cloudTag">클라우드 태그</a></li>
+		              <li><a id="dangerDocAndHos">주의 병원&의사 <span>(5)</span></a></li>
+		              <li><a id=claimBtn>사기 유형 <span>(4)</span></a></li>
+		              <li><a id="agebtn">나이<span>(6)</span></a></li>
+		              <li><a id="inforData2">기본정보(금액)<span>(4)</span></a></li>
+		              <li><a id="inforData">기본정보(기타)<span>(4)</span></a></li>
+		              <li><a id="inforData3">기본정보(기타2)<span>(4)</span></a></li>
+		            </ul>
+		        </div>
+            </div><!-- End sidebar -->
+          </div><!-- End blog sidebar -->
+        </div><!-- End .row -->
+      </div><!-- End .container -->
+    </section><!-- End Blog Section -->
+
+  </main><!-- End #main -->
+<jsp:include page="/WEB-INF/views/include/footer.jsp"/>
+<jsp:include page="/WEB-INF/views/include/staticJsp.jsp"/>
+<!-- 커스텀 그래프  --> 
+<!-- 그래프 js -->
+
+
+<script src=<c:url value="/assets/js/statistic/statistic-graph.js"/>></script>
+  <!-- 위 코드 고정 시키기   -->
+  <script type="text/javascript">
+  	document.getElementById('header').setAttribute('class', 'fixed-top')
+  </script>
+
+</body>
+
+</html>
